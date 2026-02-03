a6416f517d9260e1e95f5d98f94db7f62d8147bd
Nikhil M <nikhilm@example.com>
2026-02-03 20:24:55 +0530

    6th update

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const data = [
   {
     id: 1,
-    title:"always fresh & always crispy & always hot",
+    title:"always fresh , always crispy & always hot",
     image:"/slide1.png",
   },
   {
@@ -30,16 +30,16 @@ const Slider = () => {
   },[])
 
   return (
-    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
+    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50'>
       {/*TEXT CONTAINER*/}
-      <div className="h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
-        <h1 className='text-5xl text-center uppercase p-4 md:text-6xl xl:text-7xl'>
+      <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
+        <h1 className='text-5xl text-center uppercase p-4 md:p10 md:text-6xl xl:text-7xl'>
           {data[currentSlide].title}
         </h1>
         <button className='bg-red-500 text-white py-4 px-8'>Order Now</button>
       </div>
       {/*IMAGE CONTAINER*/}
-      <div className="w-full h-1/2 relative ">
+      <div className="w-full flex-1 relative">
         <Image src={data[currentSlide].image} alt="" fill className='object-cover'/>
       </div>
     </div>
